6efaffba67fd7c7cdb899ab5479524b90c616e7b
Anton Davydov <antondavydov.o@gmail.com>
2019-06-04 16:56:59 +0300

    Initialize project

---
+++ lib/dry/system/dependency_graph.rb
@@ -0,0 +1,10 @@
+require "dry/system/dependency_graph/version"
+
+module Dry
+  module System
+    module DependencyGraph
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/dry/system/dependency_graph/version.rb
@@ -0,0 +1,7 @@
+module Dry
+  module System
+    module DependencyGraph
+      VERSION = "0.1.0"
+    end
+  end
+end
