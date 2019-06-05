090c0c68a26ff8c02f33d477daf49e1672902521
Anton Davydov <antondavydov.o@gmail.com>
2019-06-05 15:01:57 +0300

    Create a scope of dependencies for better understanding what happening

--- lib/dry/system/dependency_graph/graph_builder.rb
+++ lib/dry/system/dependency_graph/graph_builder.rb
@@ -8,19 +8,29 @@ module Dry
           nodes = calculate_nodes(events)
           edges = calculate_edges(events, nodes)
 
-          # Create a new graph
           graph_object = GraphViz.new(:DrySystemDependencyGraph, type: :digraph)
 
-          # Create two nodes
-          nodes.each { |node| graph_object.add_nodes(*node) }
+          group_by_keys(nodes).each do |cluster_name, cluster_nodes|
+            cluster_graph = graph_object.add_graph("cluster.#{cluster_name}", label: "#{cluster_name} scope")
+            cluster_nodes.each { |node| cluster_graph.add_nodes(*node) }
+          end
+
           edges.each { |edge| graph_object.add_edges(*edge) }
+
           graph_object
         end
 
       private
 
         def calculate_nodes(events)
-          events[:registered_dependency].map { |event| [event[:class_name].name, { label: event[:key].to_s }] }
+          events[:registered_dependency].map { |event| [event[:class].name, { label: event[:key].to_s }] }
+        end
+
+        def group_by_keys(nodes)
+          nodes.group_by do |class_name, payload|
+            scope = payload[:label].split('.').first
+            scope == payload[:label] ? 'other' : scope
+          end
         end
 
         def calculate_edges(events, nodes)
