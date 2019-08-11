ed270dd24d52844e3bdb22f4fac7e6b0154754a8
Anton Davydov <antondavydov.o@gmail.com>
2019-08-11 11:59:23 +0300

    Drop unnecessary code from web part

--- lib/dry/system/dependency_graph/web.rb
+++ lib/dry/system/dependency_graph/web.rb
@@ -12,9 +12,7 @@ module Dry
 
         get '/' do
           dependency_graph = settings.container[:dependency_graph]
-
           @xdot = dependency_graph.graph.output(xdot: String)
-          @dependencies_calls = dependency_graph.dependencies_calls
 
           erb :graph
         end
