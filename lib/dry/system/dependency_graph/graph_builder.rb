ce0935a9242a3398bcd8aadedadd9dc9ec1f70bb
Anton Davydov <antondavydov.o@gmail.com>
2020-02-03 02:20:41 +0300

    Calculate nodes for dependencies outside container

--- lib/dry/system/dependency_graph/graph_builder.rb
+++ lib/dry/system/dependency_graph/graph_builder.rb
@@ -7,6 +7,12 @@ module Dry
           edges = calculate_edges(events, nodes)
           groups = calculate_groups(nodes)
 
+          nodes_outside_container = (edges.map { |e| e[:target] } - nodes.map { |n| n[:id] }).map do |class_name|
+            { id: class_name, label: class_name, shape: 'ellipse' }
+          end.uniq
+
+          nodes = nodes + nodes_outside_container
+
           { nodes: nodes, edges: edges, groups: groups }
         end
 
@@ -34,7 +40,8 @@ module Dry
         def calculate_edges(events, nodes)
           events[:resolved_dependency].flat_map do |event|
             event[:dependency_map].map do |label, key|
-              inject_class = nodes.find { |node| node[:label] == key }
+
+              inject_class = nodes.find { |node| node[:label].to_s == key.to_s }
               { target: event[:target_class].name, source: inject_class[:id], label: label }
             end
           end
